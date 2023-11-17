99cd700d4141a2fd763460ddb1d3209e7c1dbea7
NHPT <42366434+NHPT@users.noreply.github.com>
2023-11-17 15:05:45 +0800

    新增定时推送功能，优化推送消息展示格式
    简单改了改，有空重写

--- FireEyeGoldCrystal.py
+++ FireEyeGoldCrystal.py
@@ -8,6 +8,63 @@ import base64
 import urllib.parse
 from openpyxl import Workbook
 import argparse
+import schedule
+from bs4 import BeautifulSoup
+import random
+
+result=[]
+flag = False
+
+# 钉钉机器人的加签密钥
+secret = ''
+# 钉钉机器人的Webhook
+webhook = ''
+# 企业微信webhook
+wxwork_url=''
+
+def clearResult():
+    global flag
+    flag = True
+
+def get_bing_wallpapers_of_the_day():
+    # 设置用户代理标头
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36'
+    }
+
+    # 发送HTTP请求获取页面内容
+    url = 'https://bing.ioliu.cn/'
+    response = requests.get(url, headers=headers)
+
+    # 检查响应是否成功
+    if response.status_code == 200:
+        # 使用Beautiful Soup解析页面内容
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 找到所有壁纸的图片元素
+        image_elements = soup.find_all('img', class_='progressive__img')
+
+        # 获取所有壁纸的URL并添加到列表
+        wallpaper_urls = []
+        for image_element in image_elements:
+            wallpaper_url = image_element['data-progressive']
+            wallpaper_urls.append(wallpaper_url)
+
+        return wallpaper_urls
+    else:
+        print('请求失败，状态码:', response.status_code)
+        return None
+
+def get_random_bing_wallpaper():
+    # 获取一天中所有Bing壁纸的URL列表
+    bing_wallpaper_urls = get_bing_wallpapers_of_the_day()
+    
+    if bing_wallpaper_urls:
+        # 随机选择一个壁纸URL
+        random_wallpaper_url = random.choice(bing_wallpaper_urls)
+        return random_wallpaper_url
+    else:
+        return None
 
 def proxy(proxy):
     try:
@@ -26,19 +83,56 @@ def headers(header):
 # 钉钉推送
 def DingDing(msg):
     timestamp = str(round(time.time() * 1000))
-    # 钉钉机器人的加签密钥
-    secret = ''
     secret_enc = secret.encode('utf-8')
     string_to_sign = '{}\n{}'.format(timestamp, secret)
     string_to_sign_enc = string_to_sign.encode('utf-8')
     hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
     sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-    # 钉钉机器人的Webhook
-    webhook = 'https://oapi.dingtalk.com/robot/send?access_token='
     url=webhook+'&timestamp='+timestamp+'&sign='+sign
-    json={"msgtype": "text","text": {"content": msg},"isAtAll": True}
+    #json={"msgtype": "text","text": {"content": msg},"isAtAll": True}
+    msglist=[{
+                "title": "GitHub情报"+str(len(msg))+"条",
+                "picURL": get_random_bing_wallpaper()
+            }]
+    for i in range(len(msg)):
+        tmpj = {"title": msg[i]['存储库描述'],
+                "messageURL": msg[i]['存储库链接']
+                }
+        msglist.append(tmpj)
+    json ={
+    "msgtype": "feedCard",
+    "feedCard": {
+        "links": msglist
+    }
+}
     r=requests.post(url,json=json,headers=head,proxies=proxies,timeout=timeout,verify=False)
+    global result,flag
+    if flag:
+        result.clear()
+        flag = False
     #print(r.text)
+
+# 企业微信推送
+def WXWork(msg):
+    #json格式化发送的数据信息
+    if not msg:
+        return
+    res = "# GitHub情报"+str(len(msg))+"条\n"
+    for i in range(len(msg)):
+        res+=str(i+1)+". ["+msg[i]['存储库描述']+"]("+msg[i]['存储库链接']+")\n"
+    data = json.dumps({
+        "msgtype": "markdown",
+        "markdown": {
+            "content": res
+            }
+        })
+    # 指定机器人发送消息
+    resp = requests.post(wxwork_url,data,auth=('Content-Type', 'application/json'))
+    global result,flag
+    if flag:
+        result.clear()
+        flag = False
+    #print(resp.json)
 
 # 获取GitHub存储库更新信息
 def GetNewSearch():
@@ -66,8 +160,8 @@ def GetNewSearch():
     #print(init_count)
     #print("[*] total_count",total_count)
     # 设置监控阈值
-    time.sleep(60*mt)
-    result=[]
+    #time.sleep(60*mt)
+    temp=[]
     for i in range(len(SearchList)):
         try:
             github_api = "https://api.github.com/search/repositories?q="+SearchList[i]+"&sort=updated"
@@ -83,25 +177,29 @@ def GetNewSearch():
                 # 更新的存储库总在前边显示，因此只需取前N个即可
                 for i in range(newRes_num):
                     newRes=items[i]
+                    html_url=newRes.get('html_url')
                     desc=newRes.get('description')
                     if desc==None:
-                        desc="空"
+                        desc=SearchList[i] + html_url.split('/')[-1]
                     else:
                         # 过滤包含敏感词的存储库
                         if Filter(desc):
                             continue
                     #print("仓库描述：",desc,"仓库链接：",newRes.get('html_url'))
-                    html_url=newRes.get('html_url')
                     # 过滤黑名单用户
                     if FilterUser(html_url):
                         continue
                     result.append({"存储库描述":desc,"存储库链接":html_url})
+                    temp.append({"存储库描述":desc,"存储库链接":html_url})
                 time.sleep(6)
         except Exception as e:
             print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()),e)
-    print(result)
-    if result:
-        DingDing(result)
+    print(temp)
+    if temp and args.r:
+        DingDing(temp)
+        WXWork(temp)
+        #ServerChan(temp)
+        #Telegram(temp)
 
 # 获取单页中的存储库描述和链接地址
 def GetOnePageData(jsonResp,sheet):
@@ -198,7 +296,7 @@ def Telegram(msg):
     group_id = ''
     bot.send_message(chat_id=group_id, text=msg)
 
-procdesc="FireEyeGoldCrystal 是一个GitHub监控和信息收集工具，支持钉钉、Server酱和Telegram推送，过滤敏感词，查找包含关键字的所有仓库并输出到FEGC.xlsx文件     --By Infiltrator"
+procdesc="FireEyeGoldCrystal 是一个GitHub监控和信息收集工具，支持钉钉、Server酱和Telegram推送，过滤敏感词，查找包含关键字的所有仓库并输出到FEGC.xlsx文件     --By NHPT"
 parser=argparse.ArgumentParser(description=procdesc,epilog="GitHub:https://github.com/nhpt")
 
 group = parser.add_mutually_exclusive_group(required=True)
@@ -207,6 +305,8 @@ group.add_argument('-c',action='store_true',help='信息收集模式')
 
 parser.add_argument('-p',help='设置代理地址，如：http://127.0.0.1:8080')
 parser.add_argument('-t',help='设置超时时间，单位：秒')
+parser.add_argument('-r',action='store_true',help='是否实时推送')
+parser.add_argument('-d',default='09:00',help='设置每天定时推送时间，默认为：09:00，需要使用24小时格式')
 parser.add_argument('-H',help='设置HTTP请求头，json格式，如：{"X-Forwarded-For":"127.0.0.1"}')
 parser.add_argument('-mT',type=int,default=5,help='设置监控阈值，单位：分，默认5分钟')
 parser.add_argument('-iF',type=argparse.FileType('r',encoding='utf8'),help='设置关键字文件')
@@ -225,7 +325,7 @@ head = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
 }
 # 关键字列表
-SearchList=["CVE-"+str(current_year) , "免杀" , "Bypass Antivirus" , "Exploit" ,
+SearchList=["CVE-"+str(current_year) ,"CVE-"+str(current_year-1),"CVE-"+str(current_year-2), "免杀" , "Bypass Antivirus" , "Exploit" ,
  "漏洞利用", "红队", "Red Team" , "蓝队", "Blue Team" , "计算机取证" , "Computer Forensics" ,
   "应急响应" , "Emergency response" , "Penetration" , "Pentest" , "内网渗透", "网络攻防",
    "网络安全" , "主机安全" , "信息收集" , "溯源" , "工控安全" , "Industrial Control Safety" ,"ICS"]
@@ -234,7 +334,7 @@ SearchList=["CVE-"+str(current_year) , "免杀" , "Bypass Antivirus" , "Exploit"
 SensitiveWords=[]
 
 # 黑名单用户
-BlacklistUsers=["thathttp01","thatjohn0a","thatjohn01"]
+BlacklistUsers=["thathttp01","thatjohn0a","thatjohn01","redflagblog-com"]
 
 if args.p:
     proxies=proxy(args.p)
@@ -250,6 +350,12 @@ if args.sW:
     SensitiveWords=[x.strip('\n') for x in args.sW.readlines()]
 if args.c:
     GetAll()
-else:
+if args.m:
+    schedule.every(mt).minutes.do(GetNewSearch)
+    if args.d:
+        schedule.every().day.at(args.d).do(WXWork,result).tag('wxwork')
+        schedule.every().day.at(args.d).do(DingDing,result).tag('dingding')
+        schedule.every().day.at(args.d).do(clearResult).tag('clearflag')
     while 1:
-        GetNewSearch()
+        schedule.run_pending()
+        time.sleep(1)
